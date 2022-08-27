b5e1237a3ff38a90f4804b12b3d23241b9fbbd81
Ivan <dugave.ivan@gmail.com>
2022-08-27 12:28:57 -0400

    solar system ready to deploy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Main {
+    public static void main(String[] args) {
+        solarSystem inception = new solarSystem();
+
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+
+        System.out.println("the basics structure is ready");
+    }
 }
